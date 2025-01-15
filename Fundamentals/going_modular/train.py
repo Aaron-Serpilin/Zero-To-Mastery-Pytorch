1d3f3c99f443d9e14e9e5710719b888495b4b671
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-01-15 19:27:18 +0100

    Addition of argparser module for training hyperparameters

--- Fundamentals/going_modular/train.py
+++ Fundamentals/going_modular/train.py
@@ -3,20 +3,34 @@ Trains a PyTorch image classification model using device-agnostic code.
 """
 
 import os
+import argparse
 import torch
 import torchvision
 import data_setup, engine, model_builder, utils
 from torchvision import transforms
 
+# Parser
+parser = argparse.ArgumentParser(description="Enter hyperparameters.")
+
+parser.add_argument("--num_epochs", default=5, type=int, help="The number of epochs to train for")
+parser.add_argument("--batch_size", default=32, type=int, help="The number of samples per batch")
+parser.add_argument("--hidden_units", default=32, type=int, help="The number of hidden units in hidden layers")
+parser.add_argument("--learning_rate", default=0.001, type=float, help="The learning rate to use for the model")
+parser.add_argument("--train_dir", default="../data/pizza_steak_sushi/train", type=str, help="The directory file path to training data in standard image classification format")
+parser.add_argument("--test_dir", default="../data/pizza_steak_sushi/test", type=str, help="The directory file path to testing data in standard image classification format")
+
+args = parser.parse_args()
+
 # Hyper parameters
-NUM_EPOCHS = 5
-BATCH_SIZE = 32
-HIDDEN_UNITS = 10
-LEARNING_RATE = 0.001
+NUM_EPOCHS = args.num_epochs
+BATCH_SIZE = args.batch_size
+HIDDEN_UNITS = args.hidden_units
+LEARNING_RATE = args.learning_rate
 
 # Directories
-train_dir = "../data/pizza_steak_sushi/train"
-test_dir = "../data/pizza_steak_sushi/test"
+train_dir = args.train_dir
+test_dir = args.test_dir
+# print(f"[INFO]\nEpochs:{NUM_EPOCHS}\nBatch size:{BATCH_SIZE}\nHidden units:{HIDDEN_UNITS}\nLearning rate:{LEARNING_RATE}\nTrain directory:{train_dir}\nTest directory:{test_dir}\n")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
